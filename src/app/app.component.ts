9d8264111530e3f9fcd2333eecd52b1e251edaa4
jessuss00 <jesusmadro23@gmail.com>
2026-05-14 12:35:17 +0200

    footer y heade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Header } from '../components/header/header';
+import { Footer } from '../components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule, Header, Footer],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
